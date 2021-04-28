9a5cba3c84aa79052ab2624a2c84b8623d346365
Hamzehobeidat <hamzehob94@gmail.com>
2021-04-28 14:04:11 +0300

    codegnieter-multi-level-user-login

---
+++ application/controllers/Admin.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->session->userdata('logged_in') !== TRUE) {
+            redirect('Login');
+        }
+    }
+
+    function index()
+    {
+        if ($this->session->userdata('level') === '1') {
+            $this->load->view('admin_view');
+        } else {
+            echo "Access Denied!";
+        }
+    }
+}
---
+++ application/models/Login_model.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login_model extends CI_Model
+{
+
+    function check_user($username, $password)
+    {
+        $this->db->select('*');
+        $this->db->from('users_tbl');
+        $this->db->where('username', $username);
+        $this->db->where('password', $password);
+        $query = $this->db->get();
+        return $query;
+    }
+}
